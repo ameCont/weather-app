81042d3fc65e8e3c564f64cd99eac03fcac29546
ameCont <americancont7@gmail.com>
2020-05-28 14:41:58 +0300

    v0

---
+++ index.js
@@ -0,0 +1,49 @@
+const API_KEY = 'DEMO_KEY'
+const API_URL = `https://api.nasa.gov/insight_weather/?
+api_key=${API_KEY}&feedtype=json&ver=1.0`
+
+// Where the cool stuff happens
+
+//Change this if you need to
+const previousWeatherToggle = document.querySelector('.show-previous-weather');
+const previousWeather = document.querySelector('.previous-weather')
+
+previousWeatherToggle.addEventListener('click', () => {
+    previousWeather.classList.toggle('show-weather')
+})
+
+getWeather().then(sols => {
+    selectedSolIndex = sols.length - 1
+    console.log(sols)
+})
+
+function displaySelectedSol(sols) {
+    const selectedSol = sols[selectedSolIndex]
+    console.log(selectedSol)
+}
+
+function getWeather(){
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+        const {
+            sol_keys,
+            validity_checks,
+            ...solData
+        } = data
+        return Object.entries(solData).map(([sol, data]) => {
+            return {
+                sol: sol,
+                maxTemp: data.AT.mx,
+                minTemp: data.AT.mn,
+                windSpeed: data.HWS.av,
+                windDirectionDegrees:
+                data.WD.most_common.compass_degrees,
+                windDirectionCardinal:
+                data.WD.most_common.compass_point,
+                date: new Date(data.First_UTC)
+            }
+        })
+
+    })
+}
